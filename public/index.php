329f7b9d1c8ac4b44875511c5b99f692692ac46f
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-06-25 22:15:20 +0200

    jwtoken

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
 
-//header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: http://localhost:3000");
 header('Access-Control-Allow-Credentials: true');
-//header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH , DELETE, OPTIONS");
-//header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
-//header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH , DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+header("Content-Type: application/json; charset=UTF-8");
 
 use App\Kernel;
 
